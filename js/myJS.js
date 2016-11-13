f7f54f13119e0333ad6e5be52f376331c92b4294
w0650113 <ngu.hoang@selu.edu>
2016-11-13 01:56:58 -0600

    added draw toolbar

--- js/myJS.js
+++ js/myJS.js
@@ -1,3 +1,4 @@
+// Example of json latlng
 var planes = [
 		["7C6B07", -40.99497, 174.50808]
 		, ["7C6B38", -41.30269, 173.63696]
@@ -9,25 +10,35 @@ var planes = [
 		, ["C820AB", -42.08414, 173.96632]
 		, ["C820B6", -41.51285, 173.53274]
 	];
-var coolPlaces = new L.LayerGroup();
-L.marker([-41.29042, 174.78219]).bindPopup('Te Papa').addTo(coolPlaces), L.marker([-41.29437, 174.78405]).bindPopup('Embassy Theatre').addTo(coolPlaces), L.marker([-41.2895, 174.77803]).bindPopup('Michael Fowler Centre').addTo(coolPlaces), L.marker([-41.28313, 174.77736]).bindPopup('Leuven Belgin Beer Cafe').addTo(coolPlaces), L.polyline([
-    [-41.28313, 174.77736]
-    , [-41.2895, 174.77803]
-    , [-41.29042, 174.78219]
-    , [-41.29437, 174.78405]
-    ]).addTo(coolPlaces);
+
+
+
+
 var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>'
     , thunLink = '<a href="http://thunderforest.com/">Thunderforest</a>';
+
 var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     , osmAttrib = '&copy; ' + osmLink + ' Contributors'
     , landUrl = 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png'
     , thunAttrib = '&copy; ' + osmLink + ' Contributors & ' + thunLink;
+
 var osmMap = L.tileLayer(osmUrl, {
         attribution: osmAttrib
     })
     , landMap = L.tileLayer(landUrl, {
         attribution: thunAttrib
     });
+var coolPlaces = new L.LayerGroup();
+L.marker([-41.29042, 174.78219]).bindPopup('Te Papa').addTo(coolPlaces), L.marker([-41.29437,174.78405]).bindPopup('EmbassyTheatre').addTo(coolPlaces), L.marker([-41.2895,174.77803]).bindPopup('MichaelFowlerCentre').addTo(coolPlaces),
+L.marker([-41.28313,174.77736]).bindPopup('LeuvenBelginBeerCafe').addTo(coolPlaces)
+, L.polyline([
+    [-41.28313, 174.77736]
+    , [-41.2895, 174.77803]
+    , [-41.29042, 174.78219]
+    , [-41.29437, 174.78405]
+    ]).addTo(coolPlaces);
+
+
 var map = L.map('map', {
     layers: [osmMap]
 }).setView([-41.3058, 174.82082], 14);
@@ -40,6 +51,8 @@ var overlays = {
     "Interesting places": coolPlaces
 };
 L.control.layers(baseLayers, overlays).addTo(map);
+
+
 //
 // var marker = L.marker([-41.3058, 174.82082],
 // {
@@ -70,3 +83,18 @@ var polyline = L.polyline(
         , dashArray: '20,15'
         , lineJoin: 'round'
     }).addTo(map);
+		var drawItems = new L.FeatureGroup();
+		map.addLayer(drawItems);
+
+		var drawControl = new L.Control.Draw(
+			{
+				edit:{
+					featureGroup: drawItems
+				}
+			});
+		map.addControl(drawControl);
+		map.on('draw:created',function(e) {
+			var type = e.layerType,
+					layer = e.layer;
+					drawItems.addLayer(layer);
+		});
